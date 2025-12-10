4c81c733a8477541b275c6f7cb5476593b7a34ff
Nandana Bojja <bojjanandana@gmail.com>
2025-12-10 15:04:09 +0530

    Add files via upload

---
+++ BasicsJavaDay1.java
@@ -0,0 +1,20 @@
+public class BasicsJavaDay1 {
+    int intnum = 10;
+    float floatnum = 7.947f;
+    char charletter = 'N';
+    boolean booleanexample = true;
+    String name = "nandana";
+    
+    public void datatypes(){
+        System.out.println("Integer:" + intnum);
+        System.out.println("Float:" + floatnum);
+        System.out.println("Character:" + charletter);
+        System.out.println("Boolean:" + booleanexample);
+        System.out.println("String:" + name);
+    }
+    public static void main(String[] args) {
+        BasicsJavaDay1 obj = new BasicsJavaDay1();
+        obj.datatypes();
+    }
+    } 
+
